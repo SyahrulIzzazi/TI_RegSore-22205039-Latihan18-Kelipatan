43b07f6f0faf0dc0469268d8979b4924136a069e
SyahrulIzzazi <149282764+SyahrulIzzazi@users.noreply.github.com>
2023-11-05 09:06:15 +0700

    Add files via upload

---
+++ TI_RegSore22205039Latihan18Kelipatan.java
@@ -0,0 +1,16 @@
+package ti_regsore.pkg22205039.latihan18.kelipatan;
+/*
+NAMA    : Syahrul Izzazi
+NIM     : 22205039
+PRODI   : TI RegSore
+DESKRIPSI : Program menghitung kelipatan
+*/
+public class TI_RegSore22205039Latihan18Kelipatan {
+    public static void main(String[] args){
+    System.out.println("Bilangan kelipatan 3,5 dari 3,5 sampai 35:");
+        for (double i = 3.5; i <= 35; i+=3.5){
+            System.out.println(i);
+            
+        }
+    }
+}
